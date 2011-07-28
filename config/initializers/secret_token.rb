d628d71afc478b6e47c3727849a17a81a33e809a
Nishita Kapadia <nishita_mscit@yahoo.com>
2011-07-28 12:35:41 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VenueManagement::Application.config.secret_token = 'e04d9eeb84cdc676be429975dfc6fc72d3323a5117de21692926d9b2781397dd96e65abe464d9ef696267d3741bd093ff8050e57241fe894924e794e0202a4b7'
